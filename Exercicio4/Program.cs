c4949ced28d2582dac175be2f283dba3d5f452f9
Pedro Henrique dos Santos <pedrohenriquedswork@gmail.com>
2026-03-12 16:28:13 -0300

    Adicionado Exercício 4

---
+++ Exercicio4/Program.cs
@@ -0,0 +1,7 @@
+﻿// 4.Crie um programa para converter a temperatura da escala Celsius para a escala Fahrenheit
+
+double celcius = 31;
+
+double conversao = celcius * 1.8 + 32;
+
+Console.WriteLine($"Resultado da conversão: {conversao:F2}");
