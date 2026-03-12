b8d0f946be22c7952e6370a36629a33ed587b62a
Pedro Henrique dos Santos <pedrohenriquedswork@gmail.com>
2026-03-12 16:54:35 -0300

    Adicionado Exercício 6

---
+++ Exercicio6/Program.cs
@@ -0,0 +1,9 @@
+﻿// 6.Crie um programa para calcular a média harmônica das notas de um Aluno
+
+double nota1 = 8.2;
+double nota2 = 8.0;
+double nota3 = 9.5;
+
+double mediaHarmonica = 3 / (1.0 / nota1 + 1.0 / nota2 + 1.0 / nota3);
+
+Console.WriteLine($"Média harmônica entre as notas do aluno = {mediaHarmonica:F2}");
