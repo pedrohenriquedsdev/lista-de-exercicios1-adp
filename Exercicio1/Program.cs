54ed07454607cf4722f4d5189f004b6d2c65c577
Pedro Henrique dos Santos <pedrohenriquedswork@gmail.com>
2026-03-12 15:12:36 -0300

    Exercício 1 - Entrará em Futura refatoração

--- Exercicio1/Program.cs
+++ Exercicio1/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿// 1.Crie um programa para calcular o volume de uma caixa retangular
+
+// multiplique o COMPRIMENTO x LARGURA x ALTURA(PROFUNDIDADE)
+
+//Usarei double para cálculos mais específicos
+
+double comprimento = 20.50;
+double largura = 15.50;
+double altura = 10.50;
+
+double volume = comprimento * largura * altura;
+
+Console.WriteLine($"Resultado = {volume} cm³");
