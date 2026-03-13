78c3912f0b8dd138e5b6a0da5f5211f5dd7f6c45
Pedro Henrique dos Santos <pedrohenriquedswork@gmail.com>
2026-03-13 14:09:51 -0300

    Inicio da Estrutura de Repetição + Adicionado Exercício 13

---
+++ Exercicio13/Program.cs
@@ -0,0 +1,15 @@
+﻿// 13.Faça um algoritmo que leia os valores A, B, C e imprima na tela se a soma de A + B é menor que C.
+
+Console.Write("Insira o valor de A: ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.Write("Insira o valor de B: ");
+int b = int.Parse(Console.ReadLine()!);
+
+Console.Write("Insira o valor de C: ");
+int c = int.Parse(Console.ReadLine()!);
+
+if (a + b < c)
+    Console.WriteLine($"SIM, {a} + {b} é < que {c}");
+else
+    Console.WriteLine($"NÃO, {a} + {b} não é < que {c}");
