bd12e247fbfacd6be0025d4f9b3a1d9309d168f8
Pedro Henrique dos Santos <pedrohenriquedswork@gmail.com>
2026-03-13 13:58:34 -0300

    Adicionado Exercício 11

---
+++ Exercicio11/Program.cs
@@ -0,0 +1,13 @@
+﻿// 11.Escreva um algoritmo para ler o nome e a idade de uma pessoa, e exibir quantos dias de vida ela possui.
+// Considere sempre anos completos, e que um ano possui 365 dias. Ex: uma pessoa com 19 anos possui 6935 dias
+// de vida; veja um exemplo de saída: MARIA, VOCÊ JÁ VIVEU 6935 DIAS.
+
+Console.Write("Digite o seu nome: ");
+string nome = Console.ReadLine()!;
+
+Console.Write("Digite a sua idade: ");
+int idade = int.Parse(Console.ReadLine()!);
+
+int dias = idade * 365;
+
+Console.WriteLine($"{nome.ToUpper()}, você possui aproximadamente {dias} de vida");
