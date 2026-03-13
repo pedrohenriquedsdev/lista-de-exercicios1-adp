d8e3a36651d2b6f538a85417cfab42715e93fe1a
Pedro Henrique dos Santos <pedrohenriquedswork@gmail.com>
2026-03-13 15:45:35 -0300

    Adicionado Exercício 19

---
+++ Exercicio19/Program.cs
@@ -0,0 +1,10 @@
+﻿// 19.Desenvolver um algoritmo que efetue a soma de todos os números ímpares que são múltiplos de três e que se
+// encontram no conjunto dos números de 1 até 500.
+
+int soma = 0;
+
+for (int i = 1; i <= 500; i += 2) if (i % 3 == 0) soma += i;
+Console.WriteLine(soma);
+
+
+
