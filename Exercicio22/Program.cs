f2a8663b649857a99d5239a9a126dfd58f309a93
Pedro Henrique dos Santos <pedrohenriquedswork@gmail.com>
2026-03-13 16:50:28 -0300

    Adicionado Exercício 22

---
+++ Exercicio22/Program.cs
@@ -0,0 +1,19 @@
+﻿// 22.Escreva um programa que leia um número e imprima a sequência de Fibonacci até esse número.
+
+// A sequência de Fibonacci é uma sucessão infinita de números inteiros, começando por 0 e 1, 
+// onde cada termo subsequente é a soma dos dois anteriores (\(0, 1, 1, 2, 3, 5, 8, 13, 21, 34)
+
+
+int a = 0;
+int b = 1;
+
+Console.Write("Digite um núemro: ");
+int numero = int.Parse(Console.ReadLine()!);
+
+while (a <= numero)
+{
+    Console.WriteLine(a);
+    int soma = a + b;
+    a = b;
+    b = soma;
+}
