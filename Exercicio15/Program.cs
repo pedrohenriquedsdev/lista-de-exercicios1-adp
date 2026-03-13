d3f95c8667d5df86072975eae30c95f7dea43609
Pedro Henrique dos Santos <pedrohenriquedswork@gmail.com>
2026-03-13 15:20:54 -0300

    Adicionado Exercício 15

---
+++ Exercicio15/Program.cs
@@ -0,0 +1,27 @@
+﻿// 15.O IMC – Índice de Massa Corporal é um critério da Organização Mundial de Saúde para dar uma indicação sobre
+// a condição de peso de uma pessoa adulta. A fórmula é IMC = peso / (altura) 2. Elabore um algoritmo que leia o
+// peso e a altura de um adulto e mostre sua condição de acordo com a listagem abaixo:
+// ● IMC em adultos Condição
+// ● Abaixo de 18,5
+// ● Abaixo do peso
+// ● Entre 18,5 e 25 Peso normal
+// ● Entre 25 e 30
+// ● Acima do peso
+// ● Acima de 30 obeso
+
+Console.Write("Digite o seu peso: ");
+double peso = double.Parse(Console.ReadLine()!);
+
+Console.Write("Digite a sua altura: ");
+double altura = double.Parse(Console.ReadLine()!);
+
+double imc = peso / (altura * altura);
+
+if (imc < 18.5)
+    Console.WriteLine("Abaixo do peso (aa ta desnutrido)");
+else if (imc > 18.5 && imc <= 25)
+    Console.WriteLine("Peso normal (top)");
+else if (imc > 25 && imc <= 30)
+    Console.WriteLine("Acima do peso (tá gordinho)");
+else
+    Console.WriteLine("Obesidade!");
