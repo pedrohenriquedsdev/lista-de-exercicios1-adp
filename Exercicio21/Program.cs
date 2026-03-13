54684ce6d6fabbcfbba55544414286b8b765083f
Pedro Henrique dos Santos <pedrohenriquedswork@gmail.com>
2026-03-13 16:31:09 -0300

    Adicionado Exercício 21

---
+++ Exercicio21/Program.cs
@@ -0,0 +1,21 @@
+﻿// 21.Escreva um algoritmo que leia um valor inicial A e imprima a sequência de valores do cálculo de A! e o seu
+// resultado.
+// a. Ex: 5! = 5 X 4 X 3 X 2 X 1 = 120
+// b. Pesquise sobre “fatorial”
+
+Console.Write("Digite um valor A: ");
+int A = int.Parse(Console.ReadLine()!);
+
+int resultado = 1;
+string sequencia = "";
+
+for (int i = A; i >= 1; i--)
+{
+    resultado *= i;
+    if (i == 1)
+        sequencia += i;
+    else
+        sequencia += $"{i} x ";
+}
+
+Console.WriteLine($"{A}! = {sequencia} = {resultado}");
